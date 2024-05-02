39594ca52fb2a4d52a40ad384a481bfe427c8426
hyun0618 <jhh48654@gmail.com>
2024-05-02 18:29:46 +0900

    cafe.view

---
+++ project01_cafe/src/com/itwill/cafe/view/CafeMain.java
@@ -0,0 +1,68 @@
+package com.itwill.cafe.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class CafeMain {
+
+	private JFrame frame;
+	private JButton btnOrder;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CafeMain window = new CafeMain();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public CafeMain() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setSize(450, 500);
+		frame.setLocationRelativeTo(null);
+		frame.getContentPane().setLayout(null);
+		
+		
+		btnOrder = new JButton("주문하기");
+		btnOrder.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				showCafeOrder();
+				frame.dispose();
+			}
+		});
+		
+		
+		btnOrder.setFont(new Font("D2Coding", Font.PLAIN, 20));
+		btnOrder.setBounds(115, 353, 200, 50);
+		frame.getContentPane().add(btnOrder);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Cafe");
+	}
+
+	private void showCafeOrder() {
+		CafeOrderFrame.showCafeOrderFrame(frame);
+	}
+}
